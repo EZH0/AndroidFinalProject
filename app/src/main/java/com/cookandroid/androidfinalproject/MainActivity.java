8dff37240e47a9f3610763dadb6d853a025fb0b5
EZH0 <wlgh33799@gmail.com>
2024-10-31 14:39:56 +0900

    업로드

--- app/src/main/java/com/cookandroid/androidfinalproject/MainActivity.java
+++ app/src/main/java/com/cookandroid/androidfinalproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.cookandroid.androidfinalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn, btn2, btn3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btn = findViewById(R.id.btnPoop); // 똥피하기이동버튼
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), miniPoop.class);
+                startActivity(intent);
+            }
+        });
+
+        btn2 = findViewById(R.id.btnMine); // 지뢰찾기이동버튼
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), miniMine.class);
+                startActivity(intent);
+            }
         });
     }
 }
